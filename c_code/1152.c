9dc8bdbc161d8d9cde714e8a40dcb1e9754e1cf4
Yein Ahn (안예인) <54733637+ahnanne@users.noreply.github.com>
2025-03-06 16:40:46 +0900

    1152 틀린 코드

---
+++ c_code/1152.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    const int EMPTY = 0;
+    const char WHITESPACE = ' ';
+
+    char str[1000000] = {EMPTY}; // 모든 자리에 0으로 초기화
+    // scanf("%s", str);
+    // ! 공백, 탭, 개행 문자를 만나면 입력이 중단되므로 이 방법 대신 아래와 같이 받아야 됨.
+    fgets(str, sizeof(str), stdin);
+
+    int answer = 0;
+    int i;
+    char prev = WHITESPACE;
+    for (i = 0; i < sizeof(str) / sizeof(char); i++) {
+        char curr = str[i];
+        if (curr == EMPTY && prev == EMPTY) {
+            break;
+        }
+
+        if (prev == WHITESPACE && (curr != WHITESPACE && curr != '\n')) {
+            answer++;
+        }
+
+        prev = str[i];
+    }
+
+    printf("%d", answer);
+}
