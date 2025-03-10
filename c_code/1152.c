f4c28c7df7461b731cdd938be3f24cbe13034be2
Yein Ahn (안예인) <54733637+ahnanne@users.noreply.github.com>
2025-03-11 00:37:27 +0900

    1152 8ms

--- c_code/1152.c
+++ c_code/1152.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main() {
     const int EMPTY = 0;
     const char WHITESPACE = ' ';
 
-    char str[1000000] = {EMPTY}; // 모든 자리에 0으로 초기화
-    // scanf("%s", str);
-    // ! 공백, 탭, 개행 문자를 만나면 입력이 중단되므로 이 방법 대신 아래와 같이 받아야 됨.
-    fgets(str, sizeof(str), stdin);
+    char str[1000001]; // 모든 자리에 0으로 초기화 (널문자 고려하여 최대 길이 + 1)
+    fgets(str, sizeof(str), stdin); // 공백 포함 입력받기 (cf. scanf())
 
     int answer = 0;
     int i;
     char prev = WHITESPACE;
-    for (i = 0; i < sizeof(str) / sizeof(char); i++) {
+    for (i = 0; i < strlen(str); i++) {
         char curr = str[i];
-        if (curr == EMPTY && prev == EMPTY) {
-            break;
-        }
 
         if (isspace(prev) && !isspace(curr)) {
             answer++;
         }
 
-        prev = str[i];
+        prev = curr;
     }
 
     printf("%d", answer);
